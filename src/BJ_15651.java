3dfe334e260f631300218357f47fb2579fffef43
taewook Kim <codelist829@naver.com>
2025-01-15 21:33:20 +0900

    first

---
+++ src/BJ_15651.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+public class BJ_15651 {
+    static int N;
+    static int M;
+    static int[] arr = new int[10];
+    static boolean[] visited = new boolean[10];
+    
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        
+        function(0);
+        System.out.println(sb);
+    }
+    public static void function(int start){
+        if(start == M){
+            for(int i=0; i<M; i++){
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1; i<=N; i++){
+            arr[start] = i;
+            function(start+1);
+        }
+    }
+}
